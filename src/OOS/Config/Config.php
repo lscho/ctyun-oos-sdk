6b68c11cfd46577ffa306645597f7e9bb1a9ab57
lscho <me@lscho.com>
2022-10-28 20:51:18 +0800

    Update Config.php

--- src/OOS/Config/Config.php
+++ src/OOS/Config/Config.php
@@ -23,11 +23,11 @@ final class Config
      */
     /**/
                                                                              // 6.0
-    const OOS_ACCESS_ID        = 'fadd131fe11bd2b94666';                     //your id
-    const OOS_ACCESS_KEY       = '6eb36ff77239ba025332b12db49f572852c31ff4'; //your key
+    const OOS_ACCESS_ID        = '';                     //your id
+    const OOS_ACCESS_KEY       = ''; //your key
     const OOS_ENDPOINT         = 'oos-cn.ctyunapi.cn';                       //your domain
     const OOS_ENDPOINT_ACCESS  = 'oos-cn-iam.ctyunapi.cn';                   //your domain for iam
-    const OOS_TEST_BUCKET      = 'zhouyur';                                  //your bucket
+    const OOS_TEST_BUCKET      = '';                                  //your bucket
     const SIGNER_USING_V4      = true;                                       //是否使用V4签名，false为使用V2签名
     const SIGNER_USING_PAYLOAD = true;                                       //负载是否参与签名
 }
